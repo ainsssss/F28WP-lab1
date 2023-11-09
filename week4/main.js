9e0704ec6d0874ea98cb1ea3fbd318a1e9267635
ainsley <145095644+ainsssss@users.noreply.github.com>
2023-11-09 10:39:10 +0400

    Delete week4 directory

--- week4/main.js
+++
@@ -1,6 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://ainsssss.github.io/F28WP-lab1/cities1.json');
-ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-};
-ourRequest.send();
